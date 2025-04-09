d522f9ffb573a2192a9cbf5d3ce0dc20aaf4fb51
AlexDinh-coder <dttrung2703@gmail.com>
2025-04-09 22:01:02 +0700

    Add audio chapter book

--- app/src/main/java/com/example/iread/Audio/AudioActivity.java
+++ app/src/main/java/com/example/iread/Audio/AudioActivity.java
@@ -1,11 +1,16 @@
 package com.example.iread.Audio;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,6 +28,29 @@ public class AudioActivity extends AppCompatActivity {
     BottomSheetBehavior bottomSheetBehavior;
     RecyclerView recyclerView;
     ReviewAdapter reviewAdapter;
+    ImageButton imgBtnPlay,btnReplay10,btnNext10;
+    SeekBar seekBar;
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
+    int duration = 0;
+    TextView time;
+
+
+    private Runnable updateTime = new Runnable() {
+        @Override
+        public void run() {
+            if (mediaPlayer != null) {
+                int currentPosition = mediaPlayer.getCurrentPosition();
+                seekBar.setProgress(currentPosition);
+                time.setText(formatTime(currentPosition) + " / " + formatTime(duration));
+                handler.postDelayed(this, 1000);
+                if(currentPosition >= mediaPlayer.getDuration()) {
+                    imgBtnPlay.setImageResource(R.drawable.ic_play);
+                    handler.removeCallbacks(updateTime);
+                }
+            }
+        }
+    };
     //List<Review> reviewList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +58,7 @@ public class AudioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_audio);
         makeStatusBarTransparent();
         applyTopPadding();
-       LinearLayout bottomSheet = findViewById(R.id.bottomSheetContainer);
+        LinearLayout bottomSheet = findViewById(R.id.bottomSheetContainer);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
         bottomSheetBehavior.setHideable(true);
@@ -45,6 +73,60 @@ public class AudioActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerReviews);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio);
+        seekBar = findViewById(R.id.seekBar);
+        duration = mediaPlayer.getDuration();
+        seekBar.setMax(duration);
+        time = findViewById(R.id.time);
+        time.setText("00:00 / " + formatTime(duration));
+        imgBtnPlay = findViewById(R.id.btnPlayPause);
+        btnReplay10 = findViewById(R.id.btnRewind);
+        btnNext10 = findViewById(R.id.btnForward);
+
+        imgBtnPlay.setOnClickListener(v -> {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+                imgBtnPlay.setImageResource(R.drawable.ic_play);
+                handler.removeCallbacks(updateTime);
+            } else {
+                mediaPlayer.start();
+                imgBtnPlay.setImageResource(R.drawable.ic_pause_circle);
+                handler.post(updateTime);
+            }
+        });
+
+        btnReplay10.setOnClickListener(v -> {
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            if (currentPosition - 10000 >= 0) {
+                mediaPlayer.seekTo(currentPosition - 10000);
+            } else {
+                mediaPlayer.seekTo(0);
+            }
+        });
+
+        btnNext10.setOnClickListener(v -> {
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            if (currentPosition + 10000 <= mediaPlayer.getDuration()) {
+                mediaPlayer.seekTo(currentPosition + 10000);
+            } else {
+                mediaPlayer.seekTo(mediaPlayer.getDuration());
+            }
+        });
+
+        // Khi kéo SeekBar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                    time.setText(formatTime(progress) + " / " + formatTime(duration));
+                }
+            }
+
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        //mediaPlayer.start();
 //        reviewList = new ArrayList<>();
 //        reviewList.add(new Review("Trung Đình Tuấn", "Doc"));
 //        reviewList.add(new Review("tel_09409219***", "Truyện tình tiết nhanh..."));
@@ -58,6 +140,20 @@ public class AudioActivity extends AppCompatActivity {
     }
 
 
+    private String formatTime(int millis) {
+        int minutes = (millis / 1000) / 60;
+        int seconds = (millis / 1000) % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(updateTime);
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
 
 
 
